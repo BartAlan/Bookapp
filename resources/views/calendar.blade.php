895a747a80acfc91c9da238b23a931b72e36ad87
BartAlan <al4n.b4rt@gmail.com>
2023-02-17 17:22:46 +0100

    add calendar blade

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <img style="margin: auto" src="images/logo.svg" alt="logo Bookapp">
+
+    <div class="p-8">
+        <section class="ctn-event">
+            <img class="event-flag" src="" alt="">
+            <section class="ctn-event_infos">
+                <div class="event-date">1 janvier 2023</div>
+                <div class="event-country">Angleterre</div>
+                <div class="event-counter">10j</div>
+            </div>
+        </section>
+    </div>
+</x-app-layout>
